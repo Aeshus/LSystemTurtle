fb78bb6b2cba2e3e74b68285bfde67f7a399dd89
Thomas (Aeshus) <git@aeshus.com>
2026-01-31 19:30:44 -0600

    Draft some input

--- LSystemTurtle/main.c
+++ LSystemTurtle/main.c
@@ -2,6 +2,9 @@
 #include <Library/UefiLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 
+/**
+ * The main function of the LSystemTurtle.
+ */
 EFI_STATUS
 EFIAPI
 UefiMain (
@@ -11,9 +14,23 @@ UefiMain (
 {
   SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
 
-  Print(L"Welcome to L-System Turtle.");
+  BOOLEAN stop = false;
+  while (!stop) {
+    Print(L"L-System Turtle\r\n");
 
-  while (1) {} ;
+    Print(L"Select options:\r\n");
+    Print(L"1) Make New\r\n");
+    Print(L"2) Use Saved\r\n");
+
+    EFI_INPUT_KEY key;
+    SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &key);
+
+    Print(&key.UnicodeChar);
+
+    stop = true;
+  }
+
+  while (1);
 
   return EFI_SUCCESS;
 }
