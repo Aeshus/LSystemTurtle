2eb35cfb451cb4ef57e6a87c06e9ecc3367a2a2d
Thomas (Aeshus) <git@aeshus.com>
2026-02-21 00:14:35 -0500

    Implement string expansion!

--- LSystemTurtle/main.c
+++ LSystemTurtle/main.c
@@ -1,6 +1,87 @@
 #include <Uefi.h>
 #include <Library/UefiLib.h>
 #include <Library/UefiBootServicesTableLib.h>
+#include <Library/MemoryAllocationLib.h>
+
+typedef struct Rule Rule;
+struct Rule {
+  CHAR16 match;
+  UINTN len;
+  CHAR16 *replace;
+};
+
+Rule *rule_create(CHAR16 match, CHAR16 *replace) {
+  Rule *r = (Rule *)AllocatePool(sizeof(Rule));
+
+  r->match = match;
+  r->len = StrLen(replace);
+  r->replace = replace;
+
+  return r;
+}
+
+typedef struct String String;
+struct String {
+  UINTN len;
+  CHAR16 *str;
+};
+
+String *string_create(CHAR16 *str) {
+  String *s = (String *)AllocatePool(sizeof(String));
+  s->len = StrLen(str);
+
+  s->str = (CHAR16 *)AllocatePool(sizeof(CHAR16) * (s->len + 1));
+  for (UINTN i = 0; i <= s->len; i++) {
+    s->str[i] = str[i];
+  }
+
+  return s;
+}
+
+void string_free(String *str) {
+  FreePool(str->str);
+  FreePool(str);
+}
+
+String *generate_string(UINTN count, Rule *rules[count], String *input) {
+  UINTN new_size = input->len;
+  for (int i = 0; i < input->len; i++) {
+    for (int j = 0; j < count; j++) {
+      if (input->str[i] == rules[j]->match) {
+        new_size += rules[j]->len - 1;
+        break;
+      }
+    }
+  }
+
+  CHAR16 *str = AllocatePool(sizeof(CHAR16) * (new_size + 1));
+
+  int offset = 0;
+  for (int i = 0; i < input->len; i++) {
+    int matched = 0;
+
+    for (int j = 0; j < count; j++) {
+      if (input->str[i] == rules[j]->match) {
+        for (int k = 0; k < rules[j]->len; k++) {
+          str[offset++] = rules[j]->replace[k];
+        }
+        matched = 1;
+        break;
+      }
+    }
+
+    if (!matched) {
+      str[offset++] = input->str[i];
+    }
+  }
+  str[offset] = L'\0';
+
+  String *s = AllocatePool(sizeof(String));
+  s->len = new_size;
+  s->str = str;
+
+  return s;
+}
 
 /**
  * The main function of the LSystemTurtle.
@@ -14,21 +95,14 @@ UefiMain (
 {
   SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
 
-  BOOLEAN stop = false;
-  while (!stop) {
-    Print(L"L-System Turtle\r\n");
+  String *input = string_create(L"CACA");
+  Rule *rule = rule_create(L'A', L"AB");
 
-    Print(L"Select options:\r\n");
-    Print(L"1) Make New\r\n");
-    Print(L"2) Use Saved\r\n");
+  Rule *rules[1] = {rule};
+  String *output = generate_string(1, rules, input);
+  string_free(input);
 
-    EFI_INPUT_KEY key;
-    SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &key);
-
-    Print(&key.UnicodeChar);
-
-    stop = true;
-  }
+  Print(output->str);
 
   while (1);
 
